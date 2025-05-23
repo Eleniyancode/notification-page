50ddb72b299785e1c78734fb519c31d16ed1303a
Abdulrahaman Ridwan Ayinla <abdulrahamanr657@gmail.com>
2025-05-23 14:50:19 -0700

    Add: the new notification count functionality

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-const markAllEl = document.getElementById('mark-all');
+const markAllEl = document.getElementById("mark-all");
 console.log(markAllEl);
-const newNotificationsEls = [...document.querySelectorAll('.new-notification')];
-console.log(newNotificationsEls);
-const newNotificationCount = document.getElementById('new-notification-count');
+const notificationsEls = [...document.querySelectorAll(".notification")];
+console.log(notificationsEls);
+let newCount = [...document.querySelectorAll(".notification.new")].length;
+const countDisplay = document.getElementById("new-notification-count");
+countDisplay.textContent = newCount;
 
-
-const dotEl = [...document.querySelectorAll('.dot')];
+console.log(countDisplay.textContent);
+const dotEl = [...document.querySelectorAll(".dot")];
 console.log(dotEl);
 
-markAllEl.addEventListener('click', () => {
-    newNotificationsEls.forEach(n => {
-        n.style.backgroundColor = 'white';
-    });
-   
-        dotEl.forEach(d => {
-            d.style.display = 'none';
-        });
-   
-        newNotificationCount.textContent = 0;
+markAllEl.addEventListener("click", () => {
+  notificationsEls.forEach((n) => {
+    n.style.backgroundColor = "white";
+  });
+
+  dotEl.forEach((d) => {
+    d.style.display = "none";
+  });
+
+  newCount = 0
+  countDisplay.textContent = 0;
+});
+
+notificationsEls.forEach((n, i) => {
+  n.addEventListener("click", () => {
+    if (countDisplay.textContent > 0)
+    if (n.classList.contains("new")) {
+      n.classList.remove("new");
+      newCount--;
+      countDisplay.textContent = newCount;
+
+      dotEl.forEach((d, j) => {
+        if (i === j) d.style.display = "none";
+      });
+    }
+  });
 });
